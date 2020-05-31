681989a5a691816e372e14a9241665f15104e9d5
yufeng-lin <1391011897@qq.com>
2020-05-31 10:35:44 +0800

    oneinfo

---
+++ HW_one/app/src/main/java/com/example/hw_one/Linerlayout_Activity.java
@@ -0,0 +1,20 @@
+package com.example.hw_one;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class  Linerlayout_Activity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.liner_layout);
+    }
+}
+
+
+
+
+
---
+++ HW_one/app/src/main/java/com/example/hw_one/constraintlayout_Activity.java
@@ -0,0 +1,14 @@
+package com.example.hw_one;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class constraintlayout_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_layout);
+    }
+}
